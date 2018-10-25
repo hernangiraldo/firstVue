b5e709cbf1335a9f98aa6733d9da3692aab5e10d
Hernán Giraldo <hernangiraldo@Hernans-MacBook-Pro-2.local>
2018-10-25 16:18:45 -0400

    enrutamiento y estilos

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,17 @@
     </header>
     <main>
       <aside class="sidebar">
-        <ol>
-          <li v-for="post in posts" :key="post.id">
-            {{ post.title }}
-          </li>
-        </ol>
+        <div class="list" v-for="post in posts" :key="post.id">
+          <router-link
+            active-class="is-active"
+            class="link"
+            :to="{ name: 'post', params: { id: post.id } }">
+            {{post.id}}. {{post.title}}
+          </router-link>
+        </div>
       </aside>
       <div class="content">
+        <router-view></router-view>
       </div>
     </main>
   </div>
@@ -64,12 +68,19 @@ header {
 }
 main {
   display: flex;
+  position: absolute;
+    overflow: hidden;
+    bottom: 0;
+    top: 105px;
+    left: 0;
+    right: 0;
 }
 .sidebar {
   width: 30%;
   border-right: 1px solid gray;
   padding: 10px 20px;
   text-align: left;
+  overflow: scroll;
 }
 .content {
   width: 70%;
@@ -80,10 +91,20 @@ h2 {
   font-weight: normal;
   margin: 0;
 }
-li {
+.list {
   margin: 10px 0;
 }
-a {
-  color: #42b983;
+.link {
+  display: block;
+  text-decoration: none;
+  margin-bottom: 10px;
+  color: #2c3e50;
+  &--home {
+    text-transform: uppercase;
+    margin-bottom: 30px;
+  }
+  &.is-active {
+    color: #42b983;
+  }
 }
 </style>
